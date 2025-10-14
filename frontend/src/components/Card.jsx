6fa244b4e3f5dbe73f2b9f713a8b1535cd7dd1a5
Arnav Nandi <153604848+arnavcs027@users.noreply.github.com>
2025-10-14 14:15:17 +0530

    Update Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,8 +3,11 @@ import { userDataContext } from '../context/UserContext'
 
 const Card = ({image}) => {
   const {serverUrl,userData,setUserData,backendImage,setBackendImage,frontendImage,setFrontendImage,selectedImage,setSelectedImage}=useContext(userDataContext)
+  
+  const isSelected=selectedImage===image
+  
   return (
-    <div className={`w-[70px] h-[140px] lg:w-[150px] lg:h-[250px] bg-[#020220] border-2 border-[#0000ff66] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-white ${selectedImage==image?"border-4 border-white shadow-2xl shadow-blue-950":null}`} onClick={()=>{
+    <div className={`w-[70px] h-[140px] lg:w-[150px] lg:h-[250px] bg-[#020220] border-2 border-[#0000ff66] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-white ${isSelected?"border-4 border-white shadow-2xl shadow-blue-950":""}`} onClick={()=>{
       setSelectedImage(image)
       setBackendImage(null)
       setFrontendImage(null)
